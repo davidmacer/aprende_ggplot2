098f64a2b06d9bf6ae1c739b3ac0e76827934137
David Martínez <jdavid.macer@gmail.com>
2021-04-19 12:36:03 -0500

    Agrega título y etiqueta x e y en español

--- src/grafica_iris.R
+++ src/grafica_iris.R
@@ -11,5 +11,8 @@ data(iris)
 ## Creación de resultados
 p <- ggplot(data = iris, aes(x=Sepal.Length, y=Sepal.Width))
 p + geom_point(aes(colour=Species)) + 
-  scale_colour_brewer(palette = "Set1")
+  scale_colour_brewer(palette = "Set1") + 
+  xlab("Longitud del pétalo") + 
+  ylab("Ancho del pétalo") + 
+  ggtitle("Gráfica de los datos de iris")
 
