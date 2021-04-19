95b8fd0236f53ec8d501683d2f2defe8397b7570
David Martínez <jdavid.macer@gmail.com>
2021-04-19 12:29:54 -0500

    Crea versión mínima de la gráfica de iris
    Puse una paleta de colores cualquiera y aún falta darle formato

---
+++ src/grafica_iris.R
@@ -0,0 +1,15 @@
+## Carga librerías
+library(ggplot2)
+
+## Carga datos
+data(iris)
+
+## Prepocesamiento / Limpieza de datos
+
+## Análisis exploratorio de datos
+
+## Creación de resultados
+p <- ggplot(data = iris, aes(x=Sepal.Length, y=Sepal.Width))
+p + geom_point(aes(colour=Species)) + 
+  scale_colour_brewer(palette = "Set1")
+
